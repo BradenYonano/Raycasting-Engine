d5445be77f83bcefda530d093a2e50bad0eba696
Braden Yonano <byonano@yahoo.com>
2019-10-28 23:06:12 -0700

    (1.2):
    *added sphere shading code
    
    *changed space to black

--- ray41.py
+++ ray41.py
@@ -29,7 +29,7 @@ ob = [0, 0, 0, 0, 2]
 pX = [0, 0, 1, -1]
 pY = [1, -1, 0, 0]
 pZ = [0, 0, 0, 0]
-pColor = [(0, 0, 0), (0, 255, 0), (255, 0, 0), (0, 0, 255)]
+pColor = [(200, 200, 0), (0, 255, 0), (255, 0, 0), (0, 0, 255)]
 #cube center points, side lengths, and colors
 cubeX = [10]
 cubeY = [10]
@@ -42,7 +42,8 @@ sphX = [0]
 sphY = [10]
 sphZ = [0]
 sphR = [2]
-sphC = [(100, 0, 0)]
+
+sphC = [(250, 0, 0)]
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -94,12 +95,43 @@ def checkSphere(x0, y0, z0, h, k, l, sR, r, phi, theta):
 
     discrim = bb - (4 * aa * cc)
 
-    
+
     if discrim >= 0:
         return True
     else:
         return False
 
+def sphereShader(x0, y0, z0, h, k, l, sR, r, phi, theta):
+    phi = math.radians(phi)
+    theta = math.radians(theta)
+
+    a = (math.cos(phi) * math.sin(theta) * r)  # + (math.cos(phi) * math.cos(theta) * thetaH) - (math.sin(phi) * phiH)
+    b = (math.sin(phi) * math.sin(theta) * r)  # + (math.sin(phi) * math.cos(theta) * thetaH) + (math.cos(phi) * phiH)
+    c = (math.cos(theta) * r)  # - (math.sin(theta) * thetaH)
+
+    #we must now find t for the line equations to get the point(s) that they intersect at
+    #a b and c for the quadratic involving the line and the sphere
+
+    bb = ((2 * x0 * a) - (2 * h * a) + (2 * y0 * b) - (2 * k * b) + (2 * z0 * c) - (2 * l * c))
+    aa = (a * a) + (b * b) + (c * c)
+    cc = ((x0 * x0) + (y0 * y0) + (z0 * z0) + (h * h) + (k * k) + (l * l) - (2 * h * x0) - (2 * k * y0) - (2 * l * z0) - (sR * sR))
+
+    #quadratic equation
+    #its supposed to be -b + or - but I still need to implement that
+    t = ((-1 * bb) - math.sqrt((bb * bb) - (4 * aa * cc))) / (2 * aa)
+
+    #now plug t in to the parametric forms of the line to get the intersect point(s)
+    x = (t * a) + x0
+    y = (t * b) + y0
+    z = (t * c) + z0
+
+    #find distance from camera to intersect point
+    d = math.sqrt(((x0 - x) * (x0 - x)) + ((y0 - y) * (y0 - y)) + ((z0 - z) * (z0 - z)))
+
+    #now return a fraction dependant on the size of the distance
+    maxDist = 1
+
+    return (maxDist / (d * 5))
 
 #checks to make sure that the object is in front of the camera
 def inView(x0, y0, x, y, phi):
@@ -135,10 +167,10 @@ clock = pygame.time.Clock()
 
 while True:
     keys = pygame.key.get_pressed()
-    window.fill(WHITE)
+    window.fill(BLACK)
 
-    #display fps
-    fps = font.render(str(int(clock.get_fps())), True, BLACK)
+    # display fps
+    fps = font.render(str(int(clock.get_fps())), True, WHITE)
     window.blit(fps, ((wS - 25), 5))
     clock.tick(30)
 
@@ -186,23 +218,23 @@ while True:
 
 
                 if ob[u] == 0:
-                    uP = u - (objC + objS)
-                    if inView(cameraLocX, cameraLocY, pX[uP], pY[uP], curPHI):
-                        if checkPoint(cameraLocX, cameraLocY, cameraLocZ, pX[uP], pY[uP], pZ[uP], r, curPHI, curTHETA):
-                            pygame.draw.circle(window, pColor[uP], (int(curX), int(curY)), int(n / 2))
+                    if inView(cameraLocX, cameraLocY, pX[objP], pY[objP], curPHI):
+                        if checkPoint(cameraLocX, cameraLocY, cameraLocZ, pX[objP], pY[objP], pZ[objP], r, curPHI, curTHETA):
+                            pygame.draw.circle(window, pColor[objP], (int(curX), int(curY)), int(n / 2))
                     objP += 1
-                elif ob[u] == 1:
-                    uC = u - (objP + objS)
-                    if inView(cameraLocX, cameraLocY, cubeX[uC], cubeY[uC], curPHI):
-                        if checkCube():
-                            pygame.draw.circle(window, cubeC[uC], (int(curX), int(curY)), int(n / 2))
-                    objC += 1
-                elif ob[u] == 2:
-                    uS = u - (objP + objC)
 
-                    if inView(cameraLocX, cameraLocY, sphX[uS], sphY[uS], curPHI):
-                        if checkSphere(cameraLocX, cameraLocY, cameraLocZ, sphX[uS], sphY[uS], sphZ[uS], sphR[uS], r, curPHI, curTHETA):
-                            pygame.draw.circle(window, sphC[uS], (int(curX), int(curY)), int(n / 2))
+                elif ob[u] == 1:
+                    if inView(cameraLocX, cameraLocY, cubeX[objC], cubeY[objC], curPHI):
+                        if checkCube():
+                            pygame.draw.circle(window, cubeC[objC], (int(curX), int(curY)), int(n / 2))
+                    objC += 1
+
+                elif ob[u] == 2:
+                    if inView(cameraLocX, cameraLocY, sphX[objS], sphY[objS], curPHI):
+
+                        if checkSphere(cameraLocX, cameraLocY, cameraLocZ, sphX[objS], sphY[objS], sphZ[objS], sphR[objS], r, curPHI, curTHETA):
+                            sS = sphereShader(cameraLocX, cameraLocY, cameraLocZ, sphX[objS], sphY[objS], sphZ[objS], sphR[objS], r, curPHI, curTHETA)
+                            pygame.draw.circle(window, (sphC[objS][0] * sS, sphC[objS][1] * sS, sphC[objS][2] * sS), (int(curX), int(curY)), int(n / 2))
                     objS += 1
 
             curPHI -= dppPHI
